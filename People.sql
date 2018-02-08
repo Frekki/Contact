00274b09cd4d4e40306b1150524b21c2a4441f54
Frekky <sobolrip1@gmail.com>
2018-02-08 12:44:29 +0200

    Initial commit

---
+++ People.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ContactId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(MAX) NULL, 
+    [Company] NVARCHAR(MAX) NULL, 
+    [Telephone] NVARCHAR(MAX) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Client] BIT NULL, 
+    [LastCall] DATETIME NULL
+)
